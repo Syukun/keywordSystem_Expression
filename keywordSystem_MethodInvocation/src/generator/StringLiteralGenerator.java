42d750efa600e73ab39125a36a968e06df99399c
Aochi <shuaochisoft@yahoo.co.jp>
2018-11-28 12:24:18 +0900

    modification on StringLiteralGenerator

--- keywordSystem_MethodInvocation/src/generator/StringLiteralGenerator.java
+++ keywordSystem_MethodInvocation/src/generator/StringLiteralGenerator.java
@@ -2,11 +2,34 @@ package generator;
 
 import java.util.Vector;
 
-import basic.StringLiteral;
+import basic.Expression;
+import basic.PrimitiveType;
+import basic.Type;
 import vector.VectorOfElements;
 
 public class StringLiteralGenerator extends ExpressionGenerator{
-	public static Vector<StringLiteral> generateStringLiteral(int depth,String keywords){
-		return VectorOfElements.stringLiteralVector;
+
+	@Override
+	public Generator[] getSubGenerators(Type t) {
+		// TODO Auto-generated method stub
+		return new Generator[] { new StringLiteralGenerator()};
+	}
+
+	@Override
+	public Vector<Type> getAllReceiverTypeName() {
+		Vector<Type> result = new Vector<Type>();
+		result.add(new PrimitiveType("String"));
+		return result;
+	}
+
+	@Override
+	public Type[] getParameterTypes(Type t) {
+		// TODO Auto-generated method stub
+		return new Type[] {new PrimitiveType("String")};
+	}
+
+	@Override
+	public void generateWithSubExps(Expression[] subExps, Vector<Expression> result) {
+		result.addAll(VectorOfElements.stringLiteralVector);
 	}
 }
